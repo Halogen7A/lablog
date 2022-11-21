ae29934da6291237354e00113fa9b9cb06d8f0de
Halit Osman Ozgur <halitosmanozgur@hotmail.com>
2022-11-21 16:18:56 -0500

    Made the dark theme persist with localStorage

--- lablog/src/main/resources/static/js/actions.js
+++ lablog/src/main/resources/static/js/actions.js
@@ -1,11 +1,28 @@
+
+if (!localStorage.recordedTheme){
+	localStorage.setItem('recordedTheme', 'Light');
+}
+
+if (localStorage.getItem('recordedTheme') === 'Light') {
+	document.documentElement.style.setProperty("--main-color-1", "#000000");
+    document.documentElement.style.setProperty("--main-color-2", "#ffffff");
+}
+
+if (localStorage.getItem('recordedTheme') === 'Dark') {
+	document.documentElement.style.setProperty("--main-color-1", "#ffffff");
+    document.documentElement.style.setProperty("--main-color-2", "#000000");
+}
+
 function setTheme(theme) {
     if(theme === 'Dark') {
-        document.documentElement.style.setProperty("--main-color-1", "#ffffff");
-        document.documentElement.style.setProperty("--main-color-2", "#000000");
+		document.documentElement.style.setProperty("--main-color-1", "#ffffff");
+    	document.documentElement.style.setProperty("--main-color-2", "#000000");
+        localStorage.setItem('recordedTheme', 'Dark');
     }
     else if(theme === 'Light') {
-        document.documentElement.style.setProperty("--main-color-1", "#000000");
-        document.documentElement.style.setProperty("--main-color-2", "#ffffff");
+		document.documentElement.style.setProperty("--main-color-1", "#000000");
+    	document.documentElement.style.setProperty("--main-color-2", "#ffffff");
+        localStorage.setItem('recordedTheme', 'Light');
     }
     else {
         alert("Enter a valid theme!");
